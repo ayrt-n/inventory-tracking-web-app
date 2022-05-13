f20c95221d3159e1723ec8215da37c44def937b1
ayrton <ayrton.parkinson1@gmail.com>
2022-05-13 17:06:17 -0400

    Add basic CRUD methods to InventoriesController

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,2 +1,48 @@
 class InventoriesController < ApplicationController
+  def index
+    @inventories = Inventory.all
+  end
+
+  def show
+    @inventory = Inventory.find(params[:id])
+  end
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = Inventory.new(inventory_params)
+
+    if @inventory.save
+      redirect_to @inventory
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @inventory = Inventory.find(params[:id])
+  end
+
+  def update
+    @inventory = Inventory.find(params[:id])
+
+    if @inventory.update(inventory_params)
+      redirect_to @inventory
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @inventory = Inventory.find(params[:id])
+    @inventory.destroy
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name, :description, :stock)
+  end
 end
