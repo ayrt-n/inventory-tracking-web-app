02ab057d8b93eb1060193f2013801fcc317f88e7
ayrton <ayrton.parkinson1@gmail.com>
2022-05-13 16:49:35 -0400

    Generate and migrate Inventory

---
+++ db/migrate/20220513204823_create_inventories.rb
@@ -0,0 +1,11 @@
+class CreateInventories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :inventories do |t|
+      t.string :name
+      t.text :description
+      t.integer :stock
+
+      t.timestamps
+    end
+  end
+end
