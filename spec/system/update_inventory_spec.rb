51c39c362b171686d042acdb8e0995d9a2009849
ayrton <ayrton.parkinson1@gmail.com>
2022-05-14 13:13:44 -0400

    Add test for duplicate skus

--- spec/system/update_inventory_spec.rb
+++ spec/system/update_inventory_spec.rb
@@ -15,17 +15,30 @@ RSpec.describe 'Updating inventory', type: :system do
     expect(page).to have_content('999999999')
   end
 
-  scenario 'invalid inputs' do
-    inventory = create(:inventory)
-    visit edit_inventory_path(inventory)
+  feature 'invalid inputs' do
+    scenario 'empty values' do
+      inventory = create(:inventory)
+      visit edit_inventory_path(inventory)
 
-    fill_in 'SKU', with: ''
-    fill_in 'Name', with: ''
-    fill_in 'Quantity', with: ''
-    click_on 'Update Inventory'
+      fill_in 'SKU', with: ''
+      fill_in 'Name', with: ''
+      fill_in 'Quantity', with: ''
+      click_on 'Update Inventory'
 
-    expect(page).to have_content("SKU can't be blank")
-    expect(page).to have_content("Name can't be blank")
-    expect(page).to have_content("Quantity can't be blank")
+      expect(page).to have_content("SKU can't be blank")
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Quantity can't be blank")
+    end
+
+    scenario 'duplicate sku' do
+      inventory1 = create(:inventory)
+      inventory2 = create(:inventory)
+      visit edit_inventory_path(inventory2)
+
+      fill_in 'SKU', with: inventory1.sku
+      click_on 'Update Inventory'
+
+      expect(page).to have_content('SKU has already been taken')
+    end
   end
 end
