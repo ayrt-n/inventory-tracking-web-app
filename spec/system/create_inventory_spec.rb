ff88d9cd441d6c171fec28e3118cc8e6db0ef3f8
ayrton <ayrton.parkinson1@gmail.com>
2022-05-14 13:08:27 -0400

    Add test for duplicate skus

--- spec/system/create_inventory_spec.rb
+++ spec/system/create_inventory_spec.rb
@@ -11,15 +11,29 @@ RSpec.describe 'Creating inventory', type: :system do
     expect(page).to have_content('POWER ZIP HOODIE')
   end
 
-  scenario 'invalid inputs' do
-    visit new_inventory_path
-    fill_in 'SKU', with: ''
-    fill_in 'Name', with: ''
-    fill_in 'Quantity', with: ''
-    click_on 'Create Inventory'
+  feature 'invalid inputs' do
+    scenario 'empty values' do
+      visit new_inventory_path
+      fill_in 'SKU', with: ''
+      fill_in 'Name', with: ''
+      fill_in 'Quantity', with: ''
+      click_on 'Create Inventory'
 
-    expect(page).to have_content("SKU can't be blank")
-    expect(page).to have_content("Name can't be blank")
-    expect(page).to have_content("Quantity can't be blank")
+      expect(page).to have_content("SKU can't be blank")
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Quantity can't be blank")
+    end
+
+    scenario 'duplicate sku' do
+      inventory = create(:inventory)
+
+      visit new_inventory_path
+      fill_in 'SKU', with: inventory.sku
+      fill_in 'Name', with: inventory.name
+      fill_in 'Quantity', with: inventory.quantity
+      click_on 'Create Inventory'
+
+      expect(page).to have_content('SKU has already been taken')
+    end
   end
 end
