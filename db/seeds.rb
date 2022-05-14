c94c18ec395b78bb5634a46e00da902f9501592d
ayrton <ayrton.parkinson1@gmail.com>
2022-05-13 20:39:47 -0400

    Add seed inventory data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Inventory.destroy_all
+
+Inventory.create(sku: 'A2A1N-GBBC', name: 'ARRIVAL WOVEN JOGGERS', description: 'Blank for now', stock: 120)
+Inventory.create(sku: 'B2A4F-BBBB', name: 'TRAINING LONG SLEEVE CROP TOP', description: 'Blank for now', stock: 45)
+Inventory.create(sku: 'I1A9U-BBKQ', name: 'ANIMAL PRINT SLING BAG', description: 'Blank for now', stock: 60)
