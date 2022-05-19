2565f9df6ab5e17e14670920cd9325e67bf5f62c
ayrton <ayrton.parkinson1@gmail.com>
2022-05-19 14:12:03 -0400

    Add validation for sufficient inventory and #update_inventory callback

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,23 @@
 class Shipment < ApplicationRecord
   belongs_to :inventory
+  validates :quantity, numericality: { greater_than: 0 }
+  validate :check_sufficient_inventory
+  before_save :update_inventory
 
   def value
     self.quantity * self.inventory.product.price
+  end
+
+  private
+
+  def check_sufficient_inventory
+    errors.add(:quantity, 'cannot be greater than available inventory') if self.quantity > self.inventory.quantity
+  end
+
+  def update_inventory
+    shipment_amount = self.quantity
+    inventory_amount = self.inventory.quantity
+
+    self.inventory.update_attribute(:quantity, inventory_amount - shipment_amount)
   end
 end
