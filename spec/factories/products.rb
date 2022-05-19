d8ca924cb770c53257925e6031454b8bb32d048a
ayrton <ayrton.parkinson1@gmail.com>
2022-05-19 10:15:29 -0400

    Create product and product_with_inventory factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :product do
-    name { "MyString" }
-    sku { "MyString" }
-    price { 1.5 }
+    sku { Faker::Invoice.reference }
+    name { Faker::Commerce.product_name.upcase }
+    price { Faker::Number.decimal(l_digits: 2) }
+
+    factory :product_with_inventory do
+      after(:create) do |product|
+        create(:inventory, product: product)
+      end
+    end
   end
 end
