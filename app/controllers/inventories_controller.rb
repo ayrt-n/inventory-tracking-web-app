7833acf28a57406c8630d112d466fe1038719c4d
ayrton <ayrton.parkinson1@gmail.com>
2022-05-19 10:57:10 -0400

    Remove unneccessary routing methods

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -7,41 +7,6 @@ class InventoriesController < ApplicationController
     @inventory = Inventory.find(params[:id])
   end
 
-  def new
-    @inventory = Inventory.new
-  end
-
-  def create
-    @inventory = Inventory.new(inventory_params)
-
-    if @inventory.save
-      redirect_to @inventory
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit
-    @inventory = Inventory.find(params[:id])
-  end
-
-  def update
-    @inventory = Inventory.find(params[:id])
-
-    if @inventory.update(inventory_params)
-      redirect_to @inventory
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @inventory = Inventory.find(params[:id])
-    @inventory.destroy
-
-    redirect_to inventories_path
-  end
-
   private
 
   def inventory_params
