e36396f43dfadbb1b70f7ff5e278df92a534dc37
ayrton <ayrton.parkinson1@gmail.com>
2022-05-19 10:15:50 -0400

    Update inventory factory with product association

--- spec/factories/inventories.rb
+++ spec/factories/inventories.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :inventory do
-    sku { Faker::Invoice.reference }
-    name { Faker::Commerce.product_name.upcase }
+    product
     quantity { Faker::Number.between(from: 20, to: 100) }
   end
 end
