afc4fbdd16e343e9314e5b0f50d73b925b08c758
ayrton <ayrton.parkinson1@gmail.com>
2022-05-19 10:57:23 -0400

    Add shipment resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :inventories
+  resources :inventories, only: [:index, :show]
   resources :products, only: [:new, :create, :edit, :update, :destroy]
+  resources :shipments
   root 'inventories#index'
 end
