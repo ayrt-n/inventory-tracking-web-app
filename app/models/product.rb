6b01f74c194e74c6a861899e5aa0fcf1b7464429
ayrton <ayrton.parkinson1@gmail.com>
2022-05-19 09:05:36 -0400

    Add accepts_nested_attributes_for Invnetory

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :name, :sku, :price, presence: true
   has_one :inventory
+  accepts_nested_attributes_for :inventory
 
   before_create do
     self.name = self.name.upcase
