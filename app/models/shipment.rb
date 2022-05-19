d4a90a95b9ae3c0c69f6828f4585b049039ed7e8
ayrton <ayrton.parkinson1@gmail.com>
2022-05-19 15:07:23 -0400

    Add before_destroy callback to add shipment back to inventory if destroyed

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,8 +1,11 @@
 class Shipment < ApplicationRecord
   belongs_to :inventory
+
   validates :quantity, numericality: { greater_than: 0 }
   validate :check_sufficient_inventory
-  before_save :update_inventory
+
+  before_save :remove_shipment_from_inventory
+  before_destroy :add_shipment_to_inventory
 
   def value
     self.quantity * self.inventory.product.price
@@ -14,10 +17,17 @@ class Shipment < ApplicationRecord
     errors.add(:quantity, 'cannot be greater than available inventory') if self.quantity > self.inventory.quantity
   end
 
-  def update_inventory
+  def remove_shipment_from_inventory
     shipment_amount = self.quantity
     inventory_amount = self.inventory.quantity
 
     self.inventory.update_attribute(:quantity, inventory_amount - shipment_amount)
+  end
+
+  def add_shipment_to_inventory
+    shipment_amount = self.quantity
+    inventory_amount = self.inventory.quantity
+
+    self.inventory.update_attribute(:quantity, inventory_amount + shipment_amount)
   end
 end
