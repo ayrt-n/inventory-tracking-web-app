ee70bcbb8aeedb5af7d98a5fae099e9673eea34f
ayrton <ayrton.parkinson1@gmail.com>
2022-05-20 09:13:08 -0400

    Replace reference to Inventory with reference to Shipment

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,10 +3,11 @@ FactoryBot.define do
     sku { Faker::Invoice.reference }
     name { Faker::Commerce.product_name.upcase }
     price { Faker::Number.decimal(l_digits: 2) }
+    quantity { Faker::Number.between(from: 20, to: 100) }
 
-    factory :product_with_inventory do
+    factory :product_with_shipment do
       after(:create) do |product|
-        create(:inventory, product: product)
+        create(:shipment, product: product)
       end
     end
   end
