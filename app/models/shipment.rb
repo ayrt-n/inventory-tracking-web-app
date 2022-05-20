2a8ffa0125dbfab2ee48142f3b0f67815c5daa73
ayrton <ayrton.parkinson1@gmail.com>
2022-05-20 12:58:42 -0400

    Move validation and callbacks from Shipment to ShipmentItems

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -4,33 +4,7 @@ class Shipment < ApplicationRecord
 
   accepts_nested_attributes_for :shipment_items, allow_destroy: true
 
-  # validates :quantity, numericality: { greater_than: 0 }
-  # validate :check_sufficient_inventory
-
-  # before_save :remove_shipment_from_inventory
-  # before_destroy :add_shipment_to_inventory
-
-  # def value
-  #   (self.quantity * self.product.price).round(2)
-  # end
-
-  # private
-
-  # def check_sufficient_inventory
-  #   errors.add(:quantity, 'cannot be greater than available inventory') if self.quantity > self.product.quantity
-  # end
-
-  # def remove_shipment_from_inventory
-  #   shipment_amount = self.quantity
-  #   inventory_amount = self.product.quantity
-
-  #   self.product.update_attribute(:quantity, inventory_amount - shipment_amount)
-  # end
-
-  # def add_shipment_to_inventory
-  #   shipment_amount = self.quantity
-  #   inventory_amount = self.product.quantity
-
-  #   self.product.update_attribute(:quantity, inventory_amount + shipment_amount)
-  # end
+  def value
+    100
+  end
 end
