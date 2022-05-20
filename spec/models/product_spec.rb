43f70aa07cc17d1947c278c5b0a46612d8b10600
ayrton <ayrton.parkinson1@gmail.com>
2022-05-20 08:51:20 -0400

    Add model spec to test for normalizing names

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'product name lower case' do
+    it 'replaces with uppercase value' do
+      product = create(:product, name: 'kylie matte lip kit')
+      expect(product.name).to eql('KYLIE MATTE LIP KIT')
+    end
+  end
 end
