1dff52a5c9f95ffe120ac883da73f5b80c16fa05
ayrton <ayrton.parkinson1@gmail.com>
2022-05-20 08:52:14 -0400

    Combine Inventory model methods with Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 class Product < ApplicationRecord
   validates :name, :sku, :price, presence: true
   validates :sku, uniqueness: true
-  has_one :inventory, dependent: :destroy
-  accepts_nested_attributes_for :inventory
+  has_many :shipments
 
-  before_create do
+  before_save do
     self.name = self.name.upcase
+    self.quantity = self.quantity || 0
+  end
+
+  def value
+    (self.quantity * self.price).round(2)
   end
 end
