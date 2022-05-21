f79a70025a277da9331f2db1e70eb49d2e95a0be
ayrton <ayrton.parkinson1@gmail.com>
2022-05-21 10:11:15 -0400

    Update test to reflect addition of customer name to shipment

--- spec/system/create_shipment_spec.rb
+++ spec/system/create_shipment_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Creating shipment', type: :system do
   scenario 'valid inputs' do
     product = create(:product, quantity: 100)
     visit new_shipment_path
+    fill_in 'Customer name', with: 'Test Customer'
     fill_in 'Quantity', with: 100
     click_on 'Create Shipment'
 
@@ -14,19 +15,31 @@ RSpec.describe 'Creating shipment', type: :system do
     scenario 'shipment quantity less than one' do
       product = create(:product, quantity: 100)
       visit new_shipment_path
+      fill_in 'Customer name', with: 'Test Customer'
       fill_in 'Quantity', with: 0
       click_on 'Create Shipment'
 
-      expect(page).to have_content('Quantity must be greater than 0')
+      expect(page).to have_content('Shipment items quantity must be greater than 0')
     end
 
     scenario 'shipment quantity greater than inventory quantity' do
       product = create(:product, quantity: 100)
       visit new_shipment_path
+      fill_in 'Customer name', with: 'Test Customer'
       fill_in 'Quantity', with: 101
       click_on 'Create Shipment'
 
-      expect(page).to have_content('Quantity cannot be greater than available inventory')
+      expect(page).to have_content('Shipment items quantity cannot be greater than available inventory')
+    end
+
+    scenario 'no customer name provided' do
+      product = create(:product, quantity: 100)
+      visit new_shipment_path
+      fill_in 'Customer name', with: ''
+      fill_in 'Quantity', with: 50
+      click_on 'Create Shipment'
+
+      expect(page).to have_content("Customer name can't be blank")
     end
   end
 end
