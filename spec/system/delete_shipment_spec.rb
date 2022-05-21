0619365635bb646840507995522071f79da017be
ayrton <ayrton.parkinson1@gmail.com>
2022-05-21 10:10:54 -0400

    Update test to reflect change to shipment view and model

--- spec/system/delete_shipment_spec.rb
+++ spec/system/delete_shipment_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting shipment', type: :system do
   scenario 'successfully deletes shipment' do
-    product = create(:product_with_shipment)
+    shipment_items = create(:shipment_item)
+
     visit shipments_path
-    expect(page).to have_content(product.name)
+    expect(page).to have_content(shipment_items.shipment.customer_name)
 
     click_on 'Delete'
     accept_alert
 
-    expect(page).not_to have_content(product.name)
+    expect(page).not_to have_content(shipment_items.shipment.customer_name)
   end
 end
