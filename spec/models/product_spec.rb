8afe413849d9991be60467b05c1fe4436508a004
ayrton <ayrton.parkinson1@gmail.com>
2022-05-21 11:02:43 -0400

    Add testing for default quantity and #value

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  before do
+    @product = create(:product, name: 'kylie matte lip kit', quantity: '', price: 10)
+  end
+
   context 'product name lower case' do
     it 'replaces with uppercase value' do
-      product = create(:product, name: 'kylie matte lip kit')
-      expect(product.name).to eql('KYLIE MATTE LIP KIT')
+      expect(@product.name).to eq('KYLIE MATTE LIP KIT')
+    end
+  end
+
+  context 'no quantity provided' do
+    it 'replaces with default value of zero' do
+      expect(@product.quantity).to eq(0)
+    end
+  end
+
+  describe '#value' do
+    it 'calculates the value of inventory' do
+      @product.quantity = 25
+
+      expect(@product.value).to eq(250)
     end
   end
 end
