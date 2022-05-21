df0326823e0f75029fa507d52ca0d5d7c89dcd50
ayrton <ayrton.parkinson1@gmail.com>
2022-05-21 16:14:25 -0400

    Add dependent destroy for ShipmentItems

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :name, :sku, :price, presence: true
   validates :sku, uniqueness: true
-  has_many :shipment_items, inverse_of: :product
+  has_many :shipment_items, inverse_of: :product, dependent: :destroy
   has_many :shipments, through: :shipment_items
 
   before_save do
