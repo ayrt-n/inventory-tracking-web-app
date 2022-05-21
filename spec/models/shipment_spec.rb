ad28f4488cd714871a68235ae628157ed89733ea
ayrton <ayrton.parkinson1@gmail.com>
2022-05-21 11:07:45 -0400

    Add test for #value

--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Shipment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#value' do
+    it 'calculates total value of shipment' do
+      shipment = create(:shipment)
+
+      product1 = create(:product, price: 5)
+      product2 = create(:product, price: 25)
+      product3 = create(:product, price: 1)
+
+      create(:shipment_item, shipment: shipment, product: product1, quantity: 5)
+      create(:shipment_item, shipment: shipment, product: product2, quantity: 10)
+      create(:shipment_item, shipment: shipment, product: product3, quantity: 10)
+
+      expect(shipment.value).to eq(285)
+    end
+  end
 end
